12e955f8d30b2246bb3d194855b4d1356e4d0613
Yaan768 <yaan7682019@gmail.com>
2025-05-30 22:17:24 -0500

    Mejora del ejercicio 4: Validacion, Mejor identidacion y orden

--- ejercicio4.cpp
+++ ejercicio4.cpp
@@ -1,24 +1,37 @@
-#include <iostream>//Determinar si es primo o no 
+#include <iostream> //Determinar si es primo o no 
 using namespace std;
+
 int main() {
-	//Entrada
+    // Entrada
     int num; 
-	int i = 2;
+    int i = 2;
     bool primo = true;
-    cout << "Introduce un numero: ";
+
+    cout << "Introduce un numero entero positivo: ";
     cin >> num;
-    //Proceso
-    if (num<=1) primo= false;
-    else {
-    	while (i< num and primo)
-    	if (num%i==0) //Comparacion de divisivilidad exacta
-    	primo=false;
-    	else 
-    	i=i+1;
-	}
-	//Salida
-	if(primo)
-	cout<<num<<" Es primo";
-	else 
-	cout<<num<<" No es primo"; 
+
+    // Validación
+    if (num < 0) {
+        cout << "No se permiten numeros negativos." << endl;
+        return 0;
+    }
+
+    // Proceso
+    if (num <= 1) {
+        primo = false;
+    } else {
+        while (i < num && primo) {
+            if (num % i == 0) // Comparación de divisibilidad exacta
+                primo = false;
+            else 
+                i = i + 1;
+        }
+    }
+
+    // Salida
+    if (primo)
+        cout << num << " Es primo." << endl;
+    else 
+        cout << num << " No es primo." << endl;
 }
+
