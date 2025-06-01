30f65a4da6c9f09b07de3d28e3bf1057fc1f2afb
Yaan768 <yaan7682019@gmail.com>
2025-05-31 23:13:24 -0500

    Mejora del ejercicio 1: Mejora de interfaz y estructura

--- ejercicio1.cpp
+++ ejercicio1.cpp
@@ -2,12 +2,31 @@
 using namespace std;
 
 int main() {
+	//Entrada
     int n;
+    cout << "=============================" << endl;
+    cout << "  MULTIPLOS POSITIVOS DE 7  " << endl;
+    cout << "=============================" << endl;
     cout << "Ingrese la cantidad de multiplos de 7 que desea ver: ";
     cin >> n;
+    // Validación y proceso
+    if (n <= 0) {
+        cout << "Por favor ingrese un numero positivo mayor que cero." << endl;
+    } else {
+        cout << "\nLos primeros " << n << " multiplos positivos de 7 son:\n";//Salida
+        //Proceso
+        int contador = 0;
+        int numero = 1;
 
-    for (int i = 1; i <= n; i=i+1) {
-        cout << i * 7 << " ";
+        while (contador < n) {
+            if (numero % 7 == 0) {
+                cout << numero << " ";
+                contador++;
+            }
+            numero++;
+        }
+        cout << endl;
     }
-    cout << endl;
+
 }
+
